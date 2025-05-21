2bf4a90e62ca6cc8224bfc7949ed2b1a6a1d5119
villachuck <carlos.villavicencio.aguila@gmail.com>
2025-05-21 02:34:55 -0600

    some extra adjustments

--- backend/routes/list.routes.js
+++ backend/routes/list.routes.js
@@ -18,7 +18,7 @@ router.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         res.json(tasks);
     }
     catch (err) {
-        res.status(500).json({ error: 'Error al obtener tareas' });
+        res.status(500).json({ error: 'Oops!, something went wrong' });
     }
 }));
 router.post('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -29,7 +29,7 @@ router.post('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         res.status(201).json(newTask);
     }
     catch (err) {
-        res.status(500).json({ error: 'Error al crear tarea' });
+        res.status(500).json({ error: 'Oops!, something went wrong' });
     }
 }));
 const updateTaskHandler = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -38,29 +38,28 @@ const updateTaskHandler = (req, res) => __awaiter(void 0, void 0, void 0, functi
         const { task, active } = req.body;
         const updated = yield List_1.ListModel.findByIdAndUpdate(id, { task, active }, { new: true });
         if (!updated) {
-            res.status(404).json({ message: "Elemento no encontrado" });
+            res.status(404).json({ message: "Element not found" });
             return;
         }
         res.status(200).json(updated);
     }
     catch (error) {
-        res.status(500).json({ message: "Error al actualizar", error });
+        res.status(500).json({ message: "Oops!, something went wrong", error });
     }
 });
 router.put("/:id", updateTaskHandler);
-
 const deleteTaskHandler = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { id } = req.params;
         const deletedTask = yield List_1.ListModel.deleteOne({ _id: id });
         if (deletedTask.deletedCount === 0) {
-            res.status(404).json({ message: 'Elemento no encontrado' });
+            res.status(404).json({ message: 'Element not found' });
             return;
         }
-        res.status(200).json({ message: 'Elemento eliminado exitosamente' });
+        res.status(200).json({ message: 'One less thing! List deleted.' });
     }
     catch (error) {
-        res.status(500).json({ message: 'Error al eliminar el elemento' });
+        res.status(500).json({ message: 'Oops!, something went wrong' });
     }
 });
 router.delete("/:id", deleteTaskHandler);
